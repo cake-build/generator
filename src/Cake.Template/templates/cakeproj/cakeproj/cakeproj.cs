2358c2ae0a685f853b072522952f08cf778871cf
Mattias Karlsson <matkar@wcom.se>
2025-06-13 11:18:34 +0200

    Remove test code from cakeproj.cs

--- src/Cake.Template/templates/cakeproj/cakeproj/cakeproj.cs
+++ src/Cake.Template/templates/cakeproj/cakeproj/cakeproj.cs
@@ -1,22 +1,6 @@
 var target = Argument("target", "Test");
 var configuration = Argument("configuration", "Release");
 
-Setup(context =>
-{
-    if (GitHubActions.IsRunningOnGitHubActions)
-    {
-        var templateVersion = context.Argument("template-version", "");
-        if (!string.IsNullOrEmpty(templateVersion))
-        {
-            context.Information($"Running on GitHub Actions with TemplateVersion: {templateVersion}");
-            
-            // Apply template version to MSBuild settings if needed
-            var msBuildSettings = new DotNetMSBuildSettings()
-                .WithProperty("TemplateVersion", templateVersion);
-        }
-    }
-});
-
 //////////////////////////////////////////////////////////////////////
 // TASKS
 //////////////////////////////////////////////////////////////////////
